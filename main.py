532d3e0bc6aa5db3e35af2d8cf0b634e3ad9ad26
Adam Selker <aselker@openmailbox.org>
2016-12-07 14:43:55 -0500

    Added smoothing

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 #Find phase offset
 import csv #for file import
 
-filename = "main.csv"
+filename = "Center.csv"
 freq = 366 #Hz
+skipLines = 6 #Header lines
+smoothLength = 100
+
+
+rows=[]
 
 with open(filename, newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',')
+    for i in range(skipLines):
+        next(reader) #skip some lines
     for row in reader:
-        print (', '.join(row))
+        rows.append(row)
+
+
+times = []
+sig1 = []
+sig2 = []
+
+(times, sig1, sig2) = map(lambda x: map(float, x), zip(*rows)) #Unzip to get three lists
+
+
+
+def smooth(toSmooth, dist):
+    smoothed = [0] * (len(toSmooth)-dist+1)
+    for i in range(len(toSmooth)-dist+1):
+        smoothed[i] = sum (toSmooth[i:i+dist+1]) / dist
+    return smoothed
+
+sm1 = smooth(list(sig1), smoothLength)
+sm2 = smooth(list(sig2), smoothLength)
+
